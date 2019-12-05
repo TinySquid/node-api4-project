d3bfd678820ac6095fe3eac9de66332313b4ae63
Michael Nunes <miketnunes@gmail.com>
2019-12-05 12:41:02 -0800

    Refactor seed file

--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -1,29 +1,17 @@
 const bcrypt = require('bcrypt');
 
 exports.seed = function (knex) {
-  const users = [];
-
   const admin = {
     username: 'admin',
     password: bcrypt.hashSync('password', 10)
   }
 
-  users.push(admin);
+  const demo = {
+    username: 'demo',
+    password: bcrypt.hashSync('demo123', 10)
+  }
+
+  const users = [admin, demo];
 
   return knex('users').insert(users);
 };
-
-/*
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
-*/
