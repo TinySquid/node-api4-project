e7dcae4f049e4482a5308390209e091e05ea61eb
Michael Nunes <miketnunes@gmail.com>
2019-12-05 13:20:45 -0800

    Add default colors

---
+++ defaultColors.js
@@ -0,0 +1,59 @@
+const colors = [
+  {
+    color: "aliceblue",
+    hex: "#f0f8ff",
+    user_id: 1
+  },
+  {
+    color: "limegreen",
+    hex: "#99ddbc",
+    user_id: 1
+  },
+  {
+    color: "aqua",
+    hex: "#00ffff",
+    user_id: 1
+  },
+  {
+    color: "aquamarine",
+    hex: "#7fffd4",
+    user_id: 1
+  },
+  {
+    color: "lilac",
+    hex: "#9a99dd",
+    user_id: 1
+  },
+  {
+    color: "softpink",
+    hex: "#dd99ba",
+    user_id: 1
+  },
+  {
+    color: "bisque",
+    hex: "#dd9a99",
+    user_id: 1
+  },
+  {
+    color: "softyellow",
+    hex: "#dcdd99",
+    user_id: 1
+  },
+  {
+    color: "blanchedalmond",
+    hex: "#ffebcd",
+    user_id: 1
+  },
+  {
+    color: "blue",
+    hex: "#6093ca",
+    user_id: 1
+  },
+  {
+    color: "blueviolet",
+    hex: "#8a2be2",
+    user_id: 1
+  }
+];
+
+module.exports = colors;
