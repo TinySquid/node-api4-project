c0a922e63b4a7784cb424b7a527e631d3e38df65
Michael Nunes <miketnunes@gmail.com>
2019-12-05 14:30:39 -0800

    Add POST for new color

--- api/colors/colorRouter.js
+++ api/colors/colorRouter.js
@@ -1,49 +1,69 @@
 const router = require('express').Router();
 
+const colorDB = require('./dbColors');
+
 const authenticator = require('../users/authenticator');
 
-const defaultColors = require('./defaultColors');
-
-let nextId = 12;
-
+//All color routes require authentication.
 router.use(authenticator);
 
-router.get("/colors", (req, res) => {
-  res.send(defaultColors);
+router.get("/", (req, res) => {
+  const id = req.userId;
+
+  colorDB.getByUserId(id)
+    .then(colors => {
+      res.status(200).json({
+        token: req.token,
+        colors: colors
+      });
+    })
+    .catch(error => {
+      res.status(500).json({ message: "Could not get colors from database" });
+    });
 });
 
-router.post("/colors", (req, res) => {
-  if (req.body.color !== undefined && req.body.code !== undefined) {
-    const newcolor = req.body;
-    newcolor.id = nextId;
-    colors.push(newcolor);
+//POST api/colors
+router.post("/", (req, res) => {
+  const { color, code } = req.body;
+
+  if (color && code) {
+    colorDB.insert({ color: color, hex: code, user_id: req.userId })
+      .then(color => {
+        res.status(201).json({
+          token: req.token,
+          color: color
+        });
+      })
+      .catch(error => {
+        res.status(500).json({ message: "Could not add color to the database", error: error });
+      });
+  } else {
+    res.status(400).json({ message: "Please provide color and color code" });
   }
-  nextId = nextId + 1;
-  res.status(201).json(colors);
 });
 
-router.put("/colors/:id", (req, res) => {
-  if (!req.params.id)
-    res.status(400).send("Your request is missing the color id");
-  if (req.body.id === undefined || !req.body.color || !req.body.code) {
-    res
-      .status(422)
-      .send("Make sure your request body has all the fields it needs");
-  }
-  colors = colors.map(color => {
-    if (`${color.id}` === req.params.id) {
-      return req.body;
-    }
-    return color;
-  });
-  res.status(200).send(req.body);
-});
+// router.put("/colors/:id", (req, res) => {
+//   if (!req.params.id)
+//     res.status(400).send("Your request is missing the color id");
+//   if (req.body.id === undefined || !req.body.color || !req.body.code) {
+//     res
+//       .status(422)
+//       .send("Make sure your request body has all the fields it needs");
+//   }
+//   colors = colors.map(color => {
+//     if (`${color.id}` === req.params.id) {
+//       return req.body;
+//     }
+//     return color;
+//   });
+//   res.status(200).send(req.body);
+// });
 
-router.delete("/colors/:id", (req, res) => {
-  if (!req.params.id)
-    res.status(400).send("Your request is missing the color id");
-  colors = colors.filter(color => `${color.id}` !== req.params.id);
-  res.status(202).send(req.params.id);
-});
+// router.delete("/colors/:id", (req, res) => {
+//   if (!req.params.id)
+//     res.status(400).send("Your request is missing the color id");
+//   colors = colors.filter(color => `${color.id}` !== req.params.id);
+//   res.status(202).send(req.params.id);
+// });
 
 module.exports = router;
