b127a7bb051ad0bba727e9123f267ab3a8178792
Michael Nunes <miketnunes@gmail.com>
2019-12-05 16:04:13 -0800

    Changed database

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ module.exports = {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      filename: './data/users.db3',
+      filename: './data/data.db3',
     },
     pool: {
       afterCreate: (conn, done) => {
